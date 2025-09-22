cf4f396e16dd0ee218cb477434d9bf0cbf584284
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-09-22 18:09:37 -0400

    / and /style.css load client.html and style.css respectively.

---
+++ src/htmlResponses.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+
+const index = fs.readFileSync(`${__dirname}/../client/client.html`);
+const css = fs.readFileSync(`${__dirname}/../client/client.html`);
+
+const getIndex = (request, response) => {
+    response.writeHead(200, {'Content-Type' : 'text/html'});
+    response.write(index); // Write Response Body (content)
+    response.end(); // End Response
+};
+
+const getCSS = (request, response) => {
+    response.writeHead(200, {'Content-Type': 'text/css'});
+    response.write(css);
+    response.end();
+};
+
+module.exports = {
+    getIndex,
+    getCSS
+};
---
+++ src/server.js
@@ -0,0 +1,25 @@
+const http = require('http');
+const htmlHandler = require('./htmlResponses.js');
+const jsonHandler = require('./jsonResponses.js');
+
+const port = process.env.PORT || process.env.NODE_PORT || 3000;
+
+/* Handles different URLs */
+const onRequest = (request, response) => {
+  // console.log(request.url);
+  switch (request.url) {
+    case '/':
+      htmlHandler.getIndex(request, response);
+      break;
+    case '/style.css':
+      htmlHandler.getCSS(request, response);
+      break;
+    default:
+      htmlHandler.getIndex(request, response);
+      break;
+  }
+};
+
+http.createServer(onRequest).listen(port, () => {
+  console.log(`Listening on 127.0.0.1:${port}`);
+});
