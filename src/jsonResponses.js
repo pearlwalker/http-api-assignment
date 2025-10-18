e3567dcf687cff493c109ec8505929b973fca838
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-10-18 18:45:56 -0400

    respondJSON content + response.writeHead

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,7 +1,11 @@
 /**Content-Type application/json */
 
 const respondJSON = (request, response, status, object) => {
-
+    const content = JSON.stringify(object);
+    response.writeHead(status, {
+        'Content-Type': 'application/json',
+        'Content-Length': Buffer.byteLength(content, 'utf8'),
+    });
 };
 
 module.exports = {
