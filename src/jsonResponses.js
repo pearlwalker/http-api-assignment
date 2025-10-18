f2d62d24434bc5f924a683975d42acb40bb750e7
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-10-18 19:19:18 -0400

    Created empty responseJSONs + calls to respondJSON

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -15,31 +15,66 @@ const respondJSON = (request, response, status, object) => {
 };
 
 const success = (request, response) => {
+    const responseJSON = {
+        message: '',
+        id: '',
+    };
 
+    respondJSON(request, response, 200, responseJSON);
 };
 
 const badRequest = (request, response) => {
+    const responseJSON = {
+        message: '',
+        id: '',
+    };
 
+    respondJSON(request, response, 200, responseJSON);
 };
 
 const unauthorized = (request, response) => {
+    const responseJSON = {
+        message: '',
+        id: '',
+    };
 
+    respondJSON(request, response, 200, responseJSON);
 };
 
 const forbidden = (request, response) => {
+    const responseJSON = {
+        message: '',
+        id: '',
+    };
 
+    respondJSON(request, response, 200, responseJSON);
 };
 
 const internal = (request, response) => {
+    const responseJSON = {
+        message: '',
+        id: '',
+    };
 
+    respondJSON(request, response, 200, responseJSON);
 };
 
 const notImplemented = (request, response) => {
+    const responseJSON = {
+        message: '',
+        id: '',
+    };
 
+    respondJSON(request, response, 200, responseJSON);
 };
 
 const notFound = (request, response) => {
+    const responseJSON = {
+        message: '',
+        id: '',
+    };
 
+    respondJSON(request, response, 200, responseJSON);
 };
 
 module.exports = {
