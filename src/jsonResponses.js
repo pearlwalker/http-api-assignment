71f8e030c3931398f4d8dba3382e17b95d801435
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-11-20 20:06:37 -0500

    Removed type from content-type writeHead It also broke the app

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -3,7 +3,7 @@
 const respondJSON = (request, response, status, object, type = 'application/json; charset=utf-8') => {
   const content = JSON.stringify(object);
   response.writeHead(status, {
-    'Content-Type': type,
+    'Content-Type': 'application/json; charset=utf-8',
     'Content-Length': Buffer.byteLength(content, 'utf8'),
   });
 
