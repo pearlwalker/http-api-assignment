4b7028c6ab86e78601228d7cacf2c71b835b4dd3
Pearl Walker <152219481+pearlwalker@users.noreply.github.com>
2025-11-20 19:37:04 -0500

    Added type parameter to responseJSON Uses JSON as a default parameter

--- src/jsonResponses.js
+++ src/jsonResponses.js
@@ -1,9 +1,9 @@
 /** Content-Type application/json */
 
-const respondJSON = (request, response, status, object) => {
+const respondJSON = (request, response, status, object, type='application/json; charset=utf-8') => {
   const content = JSON.stringify(object);
   response.writeHead(status, {
-    'Content-Type': 'application/json; charset=utf-8',
+    'Content-Type': type,
     'Content-Length': Buffer.byteLength(content, 'utf8'),
   });
 
